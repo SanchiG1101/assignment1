c98eeb1ed8f2fdf8b28d3297c9ea4fd4d767458d
SanchiG1101 <125590007+SanchiG1101@users.noreply.github.com>
2025-01-25 00:26:56 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Topsis-sanchi-102203595",
+    version="0.1",
+    packages=find_packages(),
+    description="A Python package for implementing Topsis , TOPSIS is based on the fundamental premise that the best solution has the shortest distance from the positive-ideal solution, and the longest distance from the negative-ideal one. Alternatives are ranked with the use of an overall index calculated based on the distances from the ideal solutions.",
+   
+    author="sanchi-102203595",
+    author_email="sgoyal2_be22@thapar.edu",
+    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
